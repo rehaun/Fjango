220c7096538702966e3be597238a1f15516d09d6
rehaun <davarpr@yahoo.com>
2021-02-21 13:47:33 +0330

    models changed
    this is first branch

--- application/models.py
+++ application/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.utils import timezone
 
@@ -8,16 +9,24 @@ class User(models.Model):
     email = models.EmailField()
 
 
-class Tweet(models.Model):
+class FollowingHearts(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    following_id = models.CharField(max_length=10)
+    hearts = models.IntegerField(
+        default=1,
+        validators=[MaxValueValidator(3), MinValueValidator(1)])
 
+
+class Tweet(models.Model):
+    tweet_id = models.CharField
     tweet_text = models.CharField(max_length=300).primary_key
     pub_date = models.DateTimeField('date published')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_cm = models.BooleanField
+    parent_id = models.ForeignKey(tweet_id, on_delete= models.CASCADE)
+    
+
 
     def __str__(self):
         return self.tweet_text
 
-
-class FollowingHearts(models.Model):
-    user_id = models.ForeignKey(User, on_delete= models.CASCADE)
-    following_id = models.CharField(max_length=10)
-    hearts = models.IntegerField()
